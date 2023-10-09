f361105ec94b127a60aabd2b4e2e30c67b248971
code810 <nadirssh@code.edu.az>
2023-10-09 10:05:55 +0400

    task1ready

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+#region verilmis araliqda tek ededlerin sayi
+Console.WriteLine("Reqemleri daxil edin");
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+if (n > m)
+{
+
+    Console.WriteLine("Reqemleri duzgun daxil edin");
+}
+else
+{
+    for (int i = n; i <= m; i++)
+    {
+        if (i % 2 == 1)
+        {
+            count += 1;
+        }
+    }
+    Console.Write("Tek ededlerin sayi: ");
+    Console.WriteLine(count);
+}
+#endregion
